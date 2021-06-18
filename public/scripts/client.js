349efb9361e3b70a872fe9816b4ea8129e571f89
Andyiev <ayevdokymenmk@gmail.com>
2021-06-18 13:11:10 -0600

    Fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,31 +16,41 @@ $(document).ready(function () {
       })
   });
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461116232227
-    }
-  ]
+  const loadtweets = function() {
+    $.ajax('/tweets', { method: 'GET', dataType: "json" })
+      .then(function (data) {
+        console.log('Success: ',data);
+        renderTweets(data);
+    });
+  };
+  
+  loadtweets();
+
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461116232227
+  //   }
+  // ]
 
   const renderTweets = function (tweets) {
     //console.log("=====tweets====", tweets);
@@ -83,5 +93,5 @@ $(document).ready(function () {
   // Test / driver code (temporary)
   //console.log($tweet); // to see what it looks like
   //$('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-  renderTweets(data);
+  //renderTweets(data);
 });
